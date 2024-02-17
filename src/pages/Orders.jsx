751cf53f866c992b8c7edbe0b4d62b06842b2299
aungkhant611658 <aungkhant611658@gmail.com>
2024-02-17 20:40:47 +0630

    setup dark mode in orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -16,10 +16,48 @@ import {
 
 import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
+import { useStateContext } from "../contexts/ContextProvider";
+
+// import "./Orders.css";
 
 const Orders = () => {
+  const { currentMode } = useStateContext();
+
+  const css = `
+    .e-grid .e-headercell {
+      background-color: #333;
+      color: #fff;
+    }
+
+    .e-gridcontent .e-rowcell {
+      background-color: #333;
+      color: #fff;
+    }
+
+    .e-grid .e-rowcell:not(.e-editedbatchcell):not(.e-updatedtd),
+    .e-grid .e-detailrowcollapse:not(.e-editedbatchcell):not(.e-updatedtd),
+    .e-grid .e-detailrowexpand:not(.e-editedbatchcell):not(.e-updatedtd),
+    .e-grid .e-gridcontent .e-rowdragdrop:not(.e-editedbatchcell):not(.e-updatedtd),
+    .e-grid .e-gridheader .e-rowdragdrop:not(.e-editedbatchcell):not(.e-updatedtd),
+    .e-grid .e-emptyrow:not(.e-editedbatchcell):not(.e-updatedtd) {
+      color: white !important;
+    }
+
+    .e-grid td.e-active {
+      background-color: #888484;
+    }
+
+    .e-grid .e-row:hover .e-rowcell{
+        background-color: #6f6f6f !important;
+    }
+`;
+
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div
+      className="m-2 md:m-10 p-2 md:p-1 rounded-3xl"
+      style={{ backgroundColor: currentMode === "Light" ? "white" : "#33373e" }}
+    >
+      {currentMode === "Dark" && <style>{css}</style>}
       <Header category="Page" title="Orders" />
 
       <GridComponent
