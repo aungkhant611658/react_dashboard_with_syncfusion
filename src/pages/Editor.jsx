999bb4e9fce9fa5a78076ad1198d158a24926c8d
aungkhant611658 <aungkhant611658@gmail.com>
2024-02-22 00:32:21 +0630

    setup text editor

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,25 +1,28 @@
-import React from "react";
-import {
-  HtmlEditor,
-  Image,
-  Inject,
-  Link,
-  QuickToolbar,
-  RichTextEditorComponent,
-  Toolbar,
-} from "@syncfusion/ej2-react-richtexteditor";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import EditorToolbar, { modules, formats } from "./EditorToolbar";
+import "react-quill/dist/quill.snow.css";
 
 import { EditorData } from "../data/dummy";
 import { Header } from "../components";
 
+import "./css/Editor.css";
+
 const Editor = () => {
+  const [data, setData] = useState(EditorData);
+
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Editor" />
-      <RichTextEditorComponent>
-        <EditorData />
-        <Inject services={[Toolbar, Image, Link, HtmlEditor, QuickToolbar]} />
-      </RichTextEditorComponent>
+      <EditorToolbar />
+      <ReactQuill
+        theme="snow"
+        value={data}
+        onChange={setData}
+        className="editor-input"
+        modules={modules}
+        formats={formats}
+      />
     </div>
   );
 };
