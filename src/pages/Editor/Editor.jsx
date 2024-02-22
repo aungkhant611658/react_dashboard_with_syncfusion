b6718a5afbb3c42b0c5d84ebe726b174554cfd42
aungkhant611658 <aungkhant611658@gmail.com>
2024-02-22 12:17:57 +0630

    setup text editor

--- src/pages/Editor/Editor.jsx
+++ src/pages/Editor/Editor.jsx
@@ -3,10 +3,10 @@ import ReactQuill from "react-quill";
 import EditorToolbar, { modules, formats } from "./EditorToolbar";
 import "react-quill/dist/quill.snow.css";
 
-import { EditorData } from "../data/dummy";
-import { Header } from "../components";
+import { EditorData } from "../../data/dummy";
+import { Header } from "../../components";
 
-import "./css/Editor.css";
+import "../css/Editor.css";
 
 const Editor = () => {
   const [data, setData] = useState(EditorData);
@@ -14,15 +14,17 @@ const Editor = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Editor" />
-      <EditorToolbar />
-      <ReactQuill
-        theme="snow"
-        value={data}
-        onChange={setData}
-        className="editor-input"
-        modules={modules}
-        formats={formats}
-      />
+      <div id="text-editor">
+        <EditorToolbar />
+        <ReactQuill
+          theme="snow"
+          value={data}
+          onChange={setData}
+          className="editor-input"
+          modules={modules}
+          formats={formats}
+        />
+      </div>
     </div>
   );
 };
